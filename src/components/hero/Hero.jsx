ab2a062399f4537ccf71cc2ed3c4913fedcc5dd1
ListyaDwiSubagya <listyodwi062@gmail.com>
2024-08-01 10:25:15 +0700

    navbar

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-text container">
+            <h1>We Ensure better education for a better world</h1>
+            <p>We Ensure curriculum is designed to empower students with
+                the knowledge, skills, and experiences needed to excel in
+                the dynamic field of education
+            </p>
+            <button className='btn'>Explore more</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
